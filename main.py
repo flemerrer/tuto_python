a0eeb7dd332cd2261b46be970ca4c985ac0c3617
flemerrer <francois.lemerrer@protonmail.com>
2024-09-26 13:14:43 +0200

    save python tutorial work

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Use a breakpoint in the code line below to debug your script.
+# Press Ctrl+F8 to toggle the breakpoint.
+import GameTurn
+
+if __name__ == '__main__':
+    GameTurn.fight()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
